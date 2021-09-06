c1ae09e4589844b45a711dbe49532e8e6efb0254
Sanaulla <ssab.developer2020@gmail.com>
2021-09-06 11:36:08 +0530

    Game Update

--- Crypto-Blash/asserts/js/main.js
+++ Crypto-Blash/asserts/js/main.js
@@ -3,8 +3,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector(".grid"); // Finding HTML elements by css selector, now it returns list of all elements with class ="grid"
     const width = 8;
+    let score = 0;
     let candies = [];
-    const candyColors = [
+    const candyImages = [
         'url(asserts/images/bitcoin_crypto.png)',
         'url(asserts/images/ethereum_crypto.png)',
         'url(asserts/images/litcoin_crypto.png)',
@@ -17,24 +18,22 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < width * width; i++) {
 
             let candy = document.createElement('div'); // Creates an HTML element
-            // console.log(candy);
             grid.appendChild(candy); // Add an HTML element candy to grie
 
             candy.setAttribute("draggable", true); // Change the attribute value of an HTML element
             candy.setAttribute("id", i); // Change the attribute value of an HTML element
 
-            let randomColorIndex = Math.floor(Math.random() * candyColors.length)
-            candy.style.backgroundImage = candyColors[randomColorIndex];
+            let randomImageIndex = Math.floor(Math.random() * candyImages.length)
+            candy.style.backgroundImage = candyImages[randomImageIndex];
             candies.push(candy)
         }
     }
 
-    // console.log(createBoard());
     createBoard();
-    let colorBeingDragged;
+    let ImageBeingDragged;
     let candyBeingDragged;
     let candyBeingReplaced;
-    let colorBeingReplaced;
+    let ImageBeingReplaced;
 
     // addEventListener() method attaches an event handler to the specific element
 
@@ -50,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }));
 
     function dragStart() {
-        colorBeingDragged = this.style.backgroundImage;
+        ImageBeingDragged = this.style.backgroundImage;
         candyBeingDragged = parseInt(this.id);
     }
 
@@ -59,12 +58,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function dragDrop() {
-        colorBeingReplaced = this.style.backgroundImage;
+        ImageBeingReplaced = this.style.backgroundImage;
         candyBeingReplaced = parseInt(this.id);
-        console.log(this.id, "DragDrop");
 
-        this.style.backgroundImage = colorBeingDragged;
-        candies[candyBeingDragged].style.backgroundImage = colorBeingReplaced;
+        this.style.backgroundImage = ImageBeingDragged;
+        candies[candyBeingDragged].style.backgroundImage = ImageBeingReplaced;
     }
 
     function dragEnd() {
@@ -75,24 +73,206 @@ document.addEventListener('DOMContentLoaded', () => {
             candyBeingDragged + width,
             candyBeingDragged - width
         ]
-        console.log(candyBeingReplaced, "In drag End")
 
-        const isValidMove = validMoves.includes(candyBeingReplaced)
+        const inValidMove = (candyBeingDragged + candyBeingReplaced) % width == width - 1 &&
+            (candyBeingDragged % width == 0 || candyBeingReplaced % width == 0);
+        const isValidMove = validMoves.includes(candyBeingReplaced) && !inValidMove;
 
         if (candyBeingReplaced && isValidMove) {
             candyBeingReplaced = null;
             candyBeingDragged = null;
-            colorBeingDragged = null;
-            colorBeingReplaced = null;
-        }
-        else if (candyBeingReplaced && !isValidMove) {
-            candies[candyBeingDragged].style.backgroundImage = colorBeingDragged;
-            candies[candyBeingReplaced].style.backgroundImage = colorBeingReplaced;
+            ImageBeingDragged = null;
+            ImageBeingReplaced = null;
+        } else if (candyBeingReplaced && !isValidMove) {
+            candies[candyBeingDragged].style.backgroundImage = ImageBeingDragged;
+            candies[candyBeingReplaced].style.backgroundImage = ImageBeingReplaced;
         }
     }
-});
 
-// document.getElementById("main-container").addEventListener('click', function() {
-//     console.log(this.style.backgroundColor);
-//     console.log(this)
-// })
+    function generateRandomCandies() {
+        let len = width * (width - 1) - 1;
+        for (let i = 0; i <= len; i++) {
+            if (candies[i + width].style.backgroundImage === '') {
+                candies[i + width].style.backgroundImage = candies[i].style.backgroundImage
+                candies[i].style.backgroundImage = ''
+            }
+            // Generate candy image in 1st row which has no backgroung
+            if (i < width && candies[i].style.backgroundImage == '') {
+                candies[i].style.backgroundImage = candyImages[
+                    Math.floor(Math.random() * candyImages.length)
+                ];
+            }
+        }
+    }
+
+    function checkRow(no_of_candies) {
+        let invalidIndex = [];
+        const len = width * width - no_of_candies;
+        for (let i = width - (no_of_candies - 1); i <= len; i += width) {
+            invalidIndex.push(i, i + 1);
+            if (no_of_candies == 4) invalidIndex.push(i + 2)
+            if (no_of_candies == 5) invalidIndex.push(i + 3)
+        }
+
+        for (let i = 0; i <= len; i++) {
+            candiesList = [];
+            candiesList.push(i, i + 1, i + 2)
+            if (no_of_candies == 4) candiesList.push(i + 3)
+            if (no_of_candies == 5) candiesList.push(i + 4)
+        
+            let desiredImage = candies[i].style.backgroundImage;
+
+            if (invalidIndex.includes(i)) continue;
+
+            let match = candiesList.every(index => desiredImage != "" && candies[index].style.
+                backgroundImage == desiredImage);
+            if (match) {
+                score += no_of_candies;
+                console.log(score);
+                candiesList.forEach(index => candies[index].style.backgroundImage = "")
+            }
+        }
+    }
+
+    // function checkRowForFive() {
+    //     let invalidIndex = [];
+    //     const len = width * width - 5;
+
+    //     // Get all Inavlid corner indices
+    //     for (let i = width - 4; i <= len; i += width)
+    //         invalidIndex.push(i, i + 1, i + 2, i + 3);
+
+    //     for (let i = 0; i <= len; i++) {
+    //         let fiveCandies = [i, i + 1, i + 2, i + 3, i + 4];
+    //         let desiredImage = candies[i].style.backgroundImage;
+
+    //         if (invalidIndex.includes(i)) continue;
+
+    //         let match = fiveCandies.every(index => desiredImage != "" && candies[index].style.
+    //             backgroundImage == desiredImage);
+    //         if (match) {
+    //             score += 5;
+    //             console.log(score);
+    //             fiveCandies.forEach(index => candies[index].style.backgroundImage = "")
+    //         }
+    //     }
+    // }
+
+    // function checkRowForFour() {
+    //     let invalidIndex = [];
+    //     const len = width * width - 4;
+
+    //     // Get all Inavlid corner indices
+    //     for (let i = width - 3; i <= len; i += width)
+    //         invalidIndex.push(i, i + 1, i + 2);
+
+    //     for (let i = 0; i <= len; i++) {
+    //         let fourCandies = [i, i + 1, i + 2, i + 3];
+    //         let desiredImage = candies[i].style.backgroundImage;
+
+    //         if (invalidIndex.includes(i)) continue;
+
+    //         let match = fourCandies.every(index => desiredImage != "" && candies[index].style.
+    //             backgroundImage == desiredImage);
+    //         if (match) {
+    //             score += 4;
+    //             console.log(score);
+    //             fourCandies.forEach(index => candies[index].style.backgroundImage = "")
+    //         }
+    //     }
+    // }
+
+    // function checkRowForThree() {
+    //     let invalidIndex = [];
+    //     const len = width * width - 3
+
+    //     for (let i = width - 2; i <= len; i += width)
+    //         invalidIndex.push(i, i + 1);
+
+    //     for (let i = 0; i <= len; i++) {
+    //         let threeCandies = [i, i + 1, i + 2];
+    //         let desiredImage = candies[i].style.backgroundImage;
+
+    //         if (invalidIndex.includes(i)) continue;
+
+    //         let match = threeCandies.every(index => desiredImage != "" && candies[index].style.
+    //             backgroundImage == desiredImage);
+    //         if (match) {
+    //             score += 3;
+    //             console.log(score);
+    //             threeCandies.forEach(index => candies[index].style.backgroundImage = "")
+    //         }
+    //     }
+    // }
+
+    function checkColumnForFive() {
+
+        let len = width * (width - 4) - 1
+
+        for (let i = 0; i <= len; i++) {
+            let fiveCandies = [i, i + width, i + width * 2, i + width * 3, i + width * 4];
+            let desiredImage = candies[i].style.backgroundImage;
+
+            let match = fiveCandies.every(index => desiredImage != "" && candies[index].style.
+                backgroundImage == desiredImage);
+            if (match) {
+                score += 5;
+                console.log(score);
+                fiveCandies.forEach(index => candies[index].style.backgroundImage = "")
+            }
+        }
+    }
+
+    function checkColumnForFour() {
+
+        let len = width * (width - 3) - 1
+
+        for (let i = 0; i <= len; i++) {
+            let fourCandies = [i, i + width, i + width * 2, i + width * 3];
+            let desiredImage = candies[i].style.backgroundImage;
+
+            let match = fourCandies.every(index => desiredImage != "" && candies[index].style.
+                backgroundImage == desiredImage);
+            if (match) {
+                score += 4;
+                console.log(score);
+                fourCandies.forEach(index => candies[index].style.backgroundImage = "")
+            }
+        }
+    }
+
+    function checkColumnForThree() {
+
+        let len = width * (width - 2) - 1
+
+        for (let i = 0; i <= len; i++) {
+            let threeCandies = [i, i + width, i + width * 2];
+            let desiredImage = candies[i].style.backgroundImage;
+
+            let match = threeCandies.every(index => desiredImage != "" && candies[index].style.
+                backgroundImage == desiredImage);
+            if (match) {
+                score += 3;
+                console.log(score);
+                threeCandies.forEach(index => candies[index].style.backgroundImage = "")
+            }
+
+        }
+
+    }
+
+    function init() {
+        checkRow(5);
+        checkColumnForFive();
+        checkRow(4);
+        checkColumnForFour();
+        checkRow(3);
+        checkColumnForThree()
+        generateRandomCandies();
+    }
+    init();
+
+    window.setInterval(function () {
+        init();
+    }, 300)
+});
